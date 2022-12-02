b392f791b92fc8873c4f736ed1a015b2bf2ffb8c
lihan <702036657>
2022-12-02 14:46:25 +0800

    项目集成ureport

--- src/main/java/com/lh/note/NoteApplication.java
+++ src/main/java/com/lh/note/NoteApplication.java
@@ -1,8 +1,13 @@
 package com.lh.note;
 
+import com.bstek.ureport.console.UReportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:context.xml")
 @SpringBootApplication
 public class NoteApplication {
 
@@ -10,4 +15,11 @@ public class NoteApplication {
 		SpringApplication.run(NoteApplication.class, args);
 	}
 
+	/**
+	 * 进行注册Servlet
+	 */
+	@Bean
+	public ServletRegistrationBean buildUReportServlet() {
+		return new ServletRegistrationBean(new UReportServlet(), "/ureport/*");
+	}
 }
